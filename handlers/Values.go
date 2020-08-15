40a5277e6e5bb5b2ff71f02d43b91f09829d73d6
Elton SV <el10savio96@gmail.com>
2020-08-15 23:22:47 +0530

    Added comments

--- handlers/Values.go
+++ handlers/Values.go
@@ -7,10 +7,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Values ...
+// Values is the HTTP handler to return the local GSet's values
+// without syncing it with other nodes in a cluster
 func Values(w http.ResponseWriter, r *http.Request) {
+	// Get the local GSet values
 	set := GSet.List()
 
+	// DEBUG log in the case of successfull
+	// list indicating the set
 	log.WithFields(log.Fields{
 		"set": set,
 	}).Debug("successful gset values")
